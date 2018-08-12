87e59fb19f36ccf8ab2fb01a76c3bb217667168f
knexer <knexer@gmail.com>
2018-08-11 20:41:15 -0700

    Initial commit. Includes some example code generating a blob-mesh cube thingy. Also includes some rather messy code that uses a blob-mesh's nodes as the bones for a skinned mesh.  This part still needs a lot of work, but the guts are there!

---
+++ Assets/BoneMapper.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// Connect a spawned blob-mesh to a rendered mesh via bones and shit
+public class BoneMapper : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer;
+
+	// Use this for initialization
+	void Start () {
+		// Assume that all the child GOs are blob-mesh surface nodes
+        // TODO in practice this won't be the case... but it may be irrelevant!
+        // Create a bone for each one
+	    var bones = new Transform[transform.childCount];
+	    var bindPoses = new Matrix4x4[transform.childCount];
+	    for (int i = 0; i < transform.childCount; i++)
+	    {
+	        bones[i] = transform.GetChild(i);
+	        bindPoses[i] = bones[i].worldToLocalMatrix * transform.localToWorldMatrix;
+	    }
+
+	    skinnedMeshRenderer.bones = bones;
+	    mesh.bindposes = bindPoses;
+	    skinnedMeshRenderer.sharedMesh = mesh;
+	    skinnedMeshRenderer.updateWhenOffscreen = true; // because the bones are ragdoll and not animation driven
+
+	    // Populate bone weights for each vertex in the rendered mesh based on the three closest bones, weighted by proximity
+        var boneWeights = new BoneWeight[mesh.vertexCount];
+	    for (int i = 0; i < mesh.vertexCount; i++)
+	    {
+	        Vector3 vertex = mesh.vertices[i];
+            // Find the closest three bones.
+            // In case of perf issues, replace orderby with a lazy quicksort, and if they persist use a spatial data structure.
+            // Should be fine though - it's only n^2 log n.
+	        var closestBones = bones.Select((bone, index) => new {bone, index}).OrderBy(bone => Vector3.SqrMagnitude(vertex - bone.bone.position)).Take(3).ToArray();
+
+            // make up a weight for each bone.  Maybe it's even a useful weight!
+	        float[] weights = closestBones.Select(bone => 1 / Vector3.Distance(bone.bone.transform.position, vertex)).ToArray();
+            float totalWeight = weights.Sum();
+	        float[] normalizedWeights = weights.Select(weight => weight / totalWeight).ToArray();
+
+	        boneWeights[i] = new BoneWeight
+	        {
+	            boneIndex0 = closestBones[0].index,
+	            weight0 = normalizedWeights[0],
+	            boneIndex1 = closestBones[1].index,
+	            weight1 = normalizedWeights[1],
+	            boneIndex2 = closestBones[2].index,
+	            weight2 = normalizedWeights[2]
+	        };
+	    }
+
+	    mesh.boneWeights = boneWeights;
+	}
+
+    // Update is called once per frame
+    void LateUpdate () {
+        Debug.Log("Blob-mesh 0: " + transform.GetChild(0).position);
+        Debug.Log("Bone 0 is at: " + skinnedMeshRenderer.bones[0].position);
+        Debug.Log("Bone weight 0 is: " + skinnedMeshRenderer.sharedMesh.boneWeights[0]);
+        var bones = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            bones[i] = transform.GetChild(i);
+        }
+
+        //skinnedMeshRenderer.bones = bones;
+    }
+}
---
+++ Assets/MeshSpawner.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSpawner : MonoBehaviour
+{
+    [SerializeField] private Rigidbody MeshNode;
+    [SerializeField] private float springStrength = 10f;
+    [SerializeField] private float dampenStrength = 1f;
+    [SerializeField] private float distance = 1f;
+    [SerializeField] private int nodeCount = 3;
+    [SerializeField] private float growthFactor = 1.05f;
+    [SerializeField] private Mesh sphereMesh;
+
+	// Use this for initialization
+	void Awake ()
+	{
+        MakeCube();
+	}
+
+    private void MakeSphere()
+    {
+        // We have this convenient sphereMesh that is already mostly sphereish and should be good enough
+
+        // Make the outer skin:
+        // TODO Spawn a node at each vertex of the sphere
+        // Connect it to each of its neighbors (that have already been spawned).  Now you have the outer skin.
+
+        // Make the inner skin:
+        // For each triangle in the mesh, create another node at the middle of the face
+        // (but inset towards the center of the sphere by approximately the distance between adjacent nodes)
+        // Connect it to the inner skin nodes for each neighboring face
+        // Connect it to the nodes for the vertices on the face this was spawned from
+
+        // Add the center node and connect it to all the inner skin nodes.
+        // (We may need a different internal structure to make non-convex blobbing possible but this should be OK for now)
+        // (These connections probably will need different spring configurations)
+    }
+
+    private void MakeCube()
+    {
+        var nodes = new Rigidbody[nodeCount, nodeCount, nodeCount];
+        for (int x = 0; x < nodeCount; x++)
+        {
+            for (int y = 0; y < nodeCount; y++)
+            {
+                for (int z = 0; z < nodeCount; z++)
+                {
+                    Rigidbody node = Instantiate(MeshNode, gameObject.transform);
+                    node.transform.position += new Vector3(x, y, z) * distance;
+                    nodes[x, y, z] = node;
+                    if (x > 0) AddSpringJoint(node, nodes[x - 1, y, z]);
+                    if (y > 0) AddSpringJoint(node, nodes[x, y - 1, z]);
+                    if (z > 0) AddSpringJoint(node, nodes[x, y, z - 1]);
+                }
+            }
+        }
+    }
+
+    private void AddSpringJoint(Rigidbody node, Rigidbody otherNode)
+    {
+        var joint = node.gameObject.AddComponent<SpringJoint>();
+        joint.connectedBody = otherNode;
+        joint.spring = springStrength;
+        joint.damper = dampenStrength;
+        joint.autoConfigureConnectedAnchor = false;
+    }
+
+    private void MakeTriangle()
+    {
+        var nodes = new Rigidbody[4];
+        var relativePositons = new[]
+            {Vector3.zero, Vector3.left * distance, Vector3.forward * distance, Vector3.up * distance};
+        for (int i = 0; i < 4; i++)
+        {
+            Rigidbody node = Instantiate(MeshNode, gameObject.transform);
+            node.transform.position += relativePositons[i];
+            nodes[i] = node;
+            for (int j = 0; j < i; j++)
+            {
+                AddSpringJoint(node, nodes[j]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+	void Update ()
+	{
+	    foreach (SpringJoint joint in GetComponentsInChildren<SpringJoint>())
+	    {
+	        joint.connectedAnchor *= growthFactor;
+	    }
+	}
+}
